a141a38cee1a514d50f0610700cd07f8303a918a
Kevin Gaviria Fonnegra <desarrollo3@virtualcolors.com>
2021-08-03 07:08:49 -0500

    avances martes, madrugada

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,3 +4,24 @@ function overBodyBtn(btn){
 function outBodyBtn(btn){
     $('#img-'+btn).attr('class', 'body-img')
 }
+
+function focusSelect(select,clase){
+    var divparent = select.parentNode
+    divparent.className = clase+'-select '+clase+'-select-active'
+} 
+
+function unfocusSelect(select,clase){
+    var divparent = select.parentNode
+    divparent.className = clase+'-select'
+}
+
+function clickCotizaSeguro(btn){
+    var status = $(btn).attr('status')
+    if(status=='collapsed'){
+        $('#cotiza-seguro-wrap').attr('class', 'cotiza-seguro-expanded')
+        $('#cotiza-seguro-btn').attr('status', 'expanded')
+    }else{
+        $('#cotiza-seguro-wrap').attr('class', 'cotiza-seguro-collapsed')
+        $('#cotiza-seguro-btn').attr('status', 'collapsed')
+    }
+}
